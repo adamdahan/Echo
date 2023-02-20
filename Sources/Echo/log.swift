da5d3fd47682d6a8b9d92747a3a6974733728157
Adam Dahan <adam@hackchild.com>
2023-02-20 11:02:36 -0500

    Updates

--- Sources/Echo/log.swift
+++ Sources/Echo/log.swift
@@ -9,27 +9,37 @@ import Foundation
 import UIKit
 
 public func print(_ items: String..., fileId: String = #fileID, filename: String = #file, function : String = #function, line: Int = #line, separator: String = " ", terminator: String = "\n") {
-    #if DEBUG
-    
-        /// This could be a huge problem. What if the module the user is already using is called Echo? 
-        guard let module = fileId.split(separator: "/").first, module != "Echo" else { return }
-        Swift.print("module: \(module)")
-        let pretty = "\(URL(fileURLWithPath: filename).lastPathComponent) [#\(line)] \(function)\n\t-> "
-        let output = items.map { "\($0)" }.joined(separator: separator)
-        Swift.print(pretty+output, terminator: terminator)
-        Logger.main.log(pretty+output)
-    #else
-        Swift.print("RELEASE MODE")
-    #endif
+//    #if DEBUG
+//
+//        /// This could be a huge problem. What if the module the user is already using is called Echo?
+//        guard let module = fileId.split(separator: "/").first, module != "Echo" else { return }
+//        Swift.print("module: \(module)")
+//        let pretty = "\(URL(fileURLWithPath: filename).lastPathComponent) [#\(line)] \(function)\n\t-> "
+//        let output = items.map { "\($0)" }.joined(separator: separator)
+//        Swift.print(pretty+output, terminator: terminator)
+//        Logger.main.log(pretty+output)
+//    #else
+//        Swift.print("RELEASE MODE")
+//    #endif
+    /// This could be a huge problem. What if the module the user is already using is called Echo?
+    guard let module = fileId.split(separator: "/").first, module != "Echo" else { return }
+    Swift.print("module: \(module)")
+    let pretty = "\(URL(fileURLWithPath: filename).lastPathComponent) [#\(line)] \(function)\n\t-> "
+    let output = items.map { "\($0)" }.joined(separator: separator)
+    Swift.print(pretty+output, terminator: terminator)
+    Logger.main.log(pretty+output)
 }
 
 public func print(_ items: Any..., separator: String = " ", terminator: String = "\n") {
-    #if DEBUG
-        let output = items.map { "\($0)" }.joined(separator: separator)
-        //Swift.print(output, terminator: terminator)
-        Logger.main.log(output)
-    #else
-        Swift.print("RELEASE MODE")
-    #endif
+//    #if DEBUG
+//        let output = items.map { "\($0)" }.joined(separator: separator)
+//        //Swift.print(output, terminator: terminator)
+//        Logger.main.log(output)
+//    #else
+//        Swift.print("RELEASE MODE")
+//    #endif
+    let output = items.map { "\($0)" }.joined(separator: separator)
+    //Swift.print(output, terminator: terminator)
+    Logger.main.log(output)
 }
 
